c6c205c890c4b52ff7ed0f9932d900f635a8984f
apsteinberg <asherps@gmail.com>
2025-05-29 17:05:22 -0400

    fragpipe 23 docker file and test script

--- hpc_submission/APS028_run_fragpipe23_test.sh
+++ hpc_submission/APS028_run_fragpipe23_test.sh
@@ -19,10 +19,8 @@ workflow=${workflow_dir}/F9.workflow
 manifest=${workflow_dir}/F9.fp-manifest
 # provide paths to fragpipe config tools, python and diann
 config_tools=/data1/kentsisa/fragpipe_ondemand/fragpipe_config_tools
-config_python=/data1/kentsisa/fragpipe_ondemand/fragpipe_env/bin/python3.9
-config_diann=/admin/software/fragpipe/fragpipe-22.0/tools/diann/1.8.2_beta_8/linux/diann-1.8.1.8
 # provide out directory
-outdir=/data1/shahs3/users/preskaa/AMLproteogenomics/data/APS028_AML_PG2_analysis/fragpipe_out
+outdir=/data1/shahs3/users/preskaa/AMLproteogenomics/data/APS028_AML_PG2_analysis/fragpipe_23_test
 # tell fragpipe itself what resources are available
 # set this to the number of "cpu-tasks" set above
 threads=4
@@ -37,33 +35,12 @@ cd ${outdir}
 mkdir -p ${outdir}/diann-output
 
 # fix PG2 headers and add decoys and contaminants
-sif_path=/data1/shahs3/users/preskaa/singularity/fragpipe_22.0.sif
-philosopher=/fragpipe_bin/fragPipe-22.0/fragpipe/tools/Philosopher/philosopher-v5.1.1
-proteome=/data1/kentsisa/AML_proteogenomics/Proj_B-101-985/F1/experiment/combined.proteome.unique.fasta
-PG2_proteome=${outdir}/proteome.fasta
-index_table=${outdir}/fasta_header_index.tsv
+sif_path=/data1/shahs3/users/preskaa/singularity/fragpipe_23.0.sif
 
-# fix headers
-# need to activate for mono
-source activate /data1/kentsisa/fragpipe_ondemand/fragpipe_env
-# python /home/preskaa/fragpipe-smk/workflow/scripts/fix_pg2_headers.py ${proteome} ${PG2_proteome} ${index_table}
-
-# add decoys and contaminants
-# singularity exec -e -B /data1/shahs3:/data1/shahs3 \
-#   -B /data1/kentsisa:/data1/kentsisa ${sif_path} bash -c "
-#   ${philosopher} workspace --init --nocheck &&
-#   ${philosopher} database --custom ${PG2_proteome} --contam --contamprefix &&
-#   ${philosopher} workspace --clean --nocheck
-# "
-
-# this command launches fragpipe
-# if you want to do a "dry-run", which just tests if everything is set-up properly
-# before you launch the actual job, uncomment the dry-run option below
-fragpipe=/fragpipe_bin/fragPipe-22.0/fragpipe/bin/fragpipe
+fragpipe=/fragpipe_bin/fragPipe-23.0/fragpipe/bin/fragpipe
 # provide paths to fragpipe config tools, python and diann
 config_tools=/fragpipe_bin/fragpipe_config_tools
-config_python=/usr/bin/python3.10
-config_diann=/fragpipe_bin/diann-1.9.1/diann-linux
+config_diann=/usr/bin/diann
 
  singularity exec -e -B /data1/shahs3:/data1/shahs3 \
    -B /data1/kentsisa:/data1/kentsisa \
@@ -75,7 +52,6 @@ ${fragpipe} --headless \
   --workdir ${outdir} \
   --threads ${threads} \
   --config-diann ${config_diann} \
-  --config-python ${config_python} \
   --ram ${memory} \
-#  --dry-run
+  --dry-run
 "
