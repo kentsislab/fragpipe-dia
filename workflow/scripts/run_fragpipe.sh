f0ded5b542d83637adfddc46836f5f184610b909
apsteinberg <asherps@gmail.com>
2025-06-26 14:33:52 -0400

    trying to fix fragpipe_config_tools directory

--- workflow/scripts/run_fragpipe.sh
+++ workflow/scripts/run_fragpipe.sh
@@ -19,7 +19,7 @@ mkdir -p ${outdir}/diann-output
 ${fragpipe} --headless \
   --workflow ${workflow} \
   --manifest ${manifest} \
-  --config-tools-folder fragpipe_config_tools \
+  --config-tools-folder /usr/bin/fragpipe_config_tools \
   --workdir ${outdir} \
   --threads ${threads} \
   --config-diann ${config_diann} \
